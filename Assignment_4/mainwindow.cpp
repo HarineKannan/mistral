d3f4a864c50bcff494e5837e3467de331bf9b928
Harine Sakthe <124062083+HarineKannan@users.noreply.github.com>
2023-02-19 23:14:51 +0530

    Add files via upload

---
+++ Assignment_4/mainwindow.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include<QMessageBox>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_SUBMIT_clicked()
+{
+   QString name=ui-> lineEdit_name->text();
+   QString rollno=ui-> lineEdit_rollno->text();
+   QString email=ui-> lineEdit_email->text();
+   QString mobileno=ui-> lineEdit_mobileno->text();
+   QString age=ui-> lineEdit_age->text();
+   QString yos=ui-> lineEdit_yos->text();
+   QString college=ui-> lineEdit_college->text();
+
+   qint8 a=name.length();
+   qint8 b=rollno.length();
+   qint8 c=email.length();
+   qint8 d=mobileno.length();
+   qint8 e=age.length();
+   qint8 g=yos.length();
+   qint8 h=college.length();
+   ui->radioButton->setChecked(true);
+
+   if(a!=0 && b!=0 && c!=0 && d!=0 && e!=0  &&g!=0 && h!=0){
+        QMessageBox::information(this,"Thank You dude","🙏🏻🙏🏻🙏🏻");
+   }
+   else
+   {
+       QMessageBox::warning(this,"Hey Dude","Fill the Details");
+   }
+}
+
