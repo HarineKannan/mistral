4bb7d5fea8f5bf87b72e8cb735b37f2037813314
Harine Sakthe <124062083+HarineKannan@users.noreply.github.com>
2023-02-19 17:05:25 +0530

    Add files via upload
    Assignment 3 completed

---
+++ assignment_3/addition.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include "header.h"
+void addition(int a,int b)
+{
+printf("%d",a+b);
+}
---
+++ assignment_3/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "header.h"
+int main()
+{
+int a,b;
+scanf("%d %d",&a,&b);
+addition(a,b);
+printf("Addition is found");
+subtraction(a,b);
+printf("reverse of the string is found");
+multiplication(a,b);
+printf("odd position of the string is found");
+divison(a,b);
+printf("even position of the string is found");
+modulus(a,b);
+printf("string is printed");
+}
+
