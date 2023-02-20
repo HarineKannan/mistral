5978dbee87f981c02417506e9f97974b7e64a1b8
HarineKannan <root@ubuntu.myguest.virtualbox.org>
2023-02-20 21:03:10 +0530

    done

---
+++ assignment_2/Assignment_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+SOURCE_NAME="/home/harine/Pictures/Screenshots"
+DESTINATION_NAME="/home/harine/Documents"
+BACKUP_NAME="backup_$(date +
+ %Y-%m-%d_%H-%M-%S).tar.gz"
+tar -czf "${DESTINATION_NAME}/${BACKUP_NAME}" "${SOURCE_NAME}"
+
